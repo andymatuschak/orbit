adef75e45a6c5954221738dd2453ecfb4ca0e2d0
Andy Matuschak <andy@andymatuschak.org>
2020-06-01 09:40:29 -0700

    Support for spaced-everything-plugin as library

---
+++ packages/spaced-everything-plugin/src/run.ts
@@ -0,0 +1,46 @@
+import levelup from "levelup";
+import leveldown from "leveldown";
+import path from "path";
+
+import * as IT from "incremental-thinking";
+import {
+  getDefaultFirebaseApp,
+  MetabookFirebaseDataClient,
+  MetabookFirebaseUserClient,
+} from "metabook-client";
+import { taskCache, notePrompts } from "spaced-everything";
+import SpacedEverythingImportCache from "./importCache";
+import { createTaskCache } from "./taskCache";
+
+(async () => {
+  const app = getDefaultFirebaseApp();
+  const userClient = new MetabookFirebaseUserClient(
+    app.firestore(),
+    "x5EWk2UT56URxbfrl7djoxwxiqH2",
+  );
+  const dataClient = new MetabookFirebaseDataClient(
+    app.firestore(),
+    app.functions(),
+  );
+
+  const importCache = new SpacedEverythingImportCache(
+    levelup(leveldown("cache.db")),
+  );
+
+  const noteDirectory = process.argv[2];
+  const noteFilenames = await IT.listNoteFiles(noteDirectory);
+  console.log(`Found ${noteFilenames.length} notes in ${noteDirectory}`);
+  const noteTaskSource = notePrompts.createTaskSource(
+    noteFilenames.map((filename) => path.join(noteDirectory, filename)),
+  );
+  const orbitTaskSink = createTaskCache(userClient, dataClient, importCache);
+
+  await taskCache.updateTaskCache(orbitTaskSink, noteTaskSource);
+
+  await importCache.close();
+})()
+  .then(() => {
+    console.log("Done");
+    process.exit(0);
+  })
+  .catch(console.error);
