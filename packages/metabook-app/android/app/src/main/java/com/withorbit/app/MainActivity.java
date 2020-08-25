aab96fdf7b90962ef47e5dbb56222235d001c86f
Andy Matuschak <andy@andymatuschak.org>
2020-08-25 11:16:42 -0700

    First pass on Android support

--- packages/metabook-app/android/app/src/main/java/com/withorbit/app/MainActivity.java
+++ packages/metabook-app/android/app/src/main/java/com/withorbit/app/MainActivity.java
@@ -1,9 +1,9 @@
-package media.metabook.client;
+package com.withorbit.app;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+// import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -16,7 +16,7 @@ public class MainActivity extends ReactActivity {
         return "main";
     }
 
-    @Override
+/*    @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
         return new ReactActivityDelegate(this, getMainComponentName()) {
             @Override
@@ -24,5 +24,5 @@ public class MainActivity extends ReactActivity {
                 return new RNGestureHandlerEnabledRootView(MainActivity.this);
             }
         };
-    }
+    }*/
 }
--- packages/metabook-app/android/app/src/main/java/com/withorbit/app/MainApplication.java
+++ packages/metabook-app/android/app/src/main/java/com/withorbit/app/MainApplication.java
@@ -1,4 +1,4 @@
-package media.metabook.client;
+package com.withorbit.app;
 
 import android.app.Application;
 import android.net.Uri;
@@ -9,7 +9,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import media.metabook.client.generated.BasePackageList;
+import com.withorbit.app.generated.BasePackageList;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
@@ -40,7 +40,12 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       List<ReactPackage> packages = new PackageList(this).getPackages();
-      packages.add(new ModuleRegistryAdapter(mModuleRegistryProvider));
+
+      // Add unimodules
+      List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
+        new ModuleRegistryAdapter(mModuleRegistryProvider)
+      );
+      packages.addAll(unimodules);
       return packages;
     }
 
