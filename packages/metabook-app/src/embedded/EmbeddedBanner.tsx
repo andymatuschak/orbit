e56b9b92a7024913d5a9b1c9bd238bde833e49c5
Andy Matuschak <andy@andymatuschak.org>
2020-10-08 14:34:24 -0700

    Trying different banner styling

--- packages/metabook-app/src/embedded/EmbeddedBanner.tsx
+++ packages/metabook-app/src/embedded/EmbeddedBanner.tsx
@@ -21,8 +21,8 @@ function getBannerMessage({
 }: EmbeddedBannerMessageInputs): string {
   if (!isSignedIn && completePromptCount === 0) {
     return sizeClass === "regular"
-      ? "Quickly review what you just read"
-      : "Quick review session";
+      ? "Quickly review what you just read."
+      : "Quick review session:";
   } else {
     if (completePromptCount < totalPromptCount) {
       return `Review: ${totalPromptCount - completePromptCount} prompts left`;
@@ -42,14 +42,13 @@ export default function EmbeddedBanner(props: EmbeddedBannerProps) {
         flexDirection: "row",
         alignItems: "center",
         height: styles.layout.gridUnit * 6,
-        backgroundColor: palette.secondaryBackgroundColor,
         zIndex: 1,
       }}
     >
       <Text
         style={[
           styles.type.label.layoutStyle,
-          { color: palette.accentColor, flexGrow: 1 },
+          { color: styles.colors.white, flexGrow: 1 },
         ]}
       >
         {getBannerMessage(props)}
