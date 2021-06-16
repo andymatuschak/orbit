bcd4be94ebc39d09db9ad8527c28f24815e46574
Andy Matuschak <andy@andymatuschak.org>
2021-06-15 23:12:23 -0700

    Updating prettier and typescript-eslint

--- packages/anki-import/src/__fixtures__/testAnkiData.ts
+++ packages/anki-import/src/__fixtures__/testAnkiData.ts
@@ -11,31 +11,31 @@ import {
   ReviewLogType,
 } from "../ankiPkg";
 
-export const testBasicModel: Model = ({
+export const testBasicModel: Model = {
   id: "101",
   type: ModelType.Standard,
   flds: [{ name: "Front" } as ModelField, { name: "Back" } as ModelField],
   tmpls: [{} as CardTemplate],
-} as Partial<Model>) as Model;
+} as Partial<Model> as Model;
 
-export const testCollection: Collection = ({
+export const testCollection: Collection = {
   models: { [testBasicModel.id]: testBasicModel },
-} as Partial<Collection>) as Collection;
+} as Partial<Collection> as Collection;
 
-export const testNote: Note = ({
+export const testNote: Note = {
   id: 100,
   mid: Number.parseInt(testBasicModel.id),
   flds: `Test Q\x1fTest A`,
-} as Partial<Note>) as Note;
+} as Partial<Note> as Note;
 
-export const testCard: Card = ({
+export const testCard: Card = {
   id: 500,
   mod: 700,
   nid: testNote.id,
-} as Partial<Card>) as Card;
+} as Partial<Card> as Card;
 
-export const testLog: Log = ({
+export const testLog: Log = {
   id: 550,
   type: ReviewLogType.Learn,
   ease: LearnEaseRating.Easy,
-} as Partial<Log>) as Log;
+} as Partial<Log> as Log;
