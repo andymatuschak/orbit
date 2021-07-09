45ca801a1b86f75393df565c637722523a56d363
Andy Matuschak <andy@andymatuschak.org>
2021-07-09 10:40:14 -0700

    Exporting OrbitStoreWeb

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -1,3 +1,4 @@
 export type { OrbitStore } from "./orbitStore";
 export { OrbitStoreFS } from "./orbitStores/orbitStoreFS";
+export { OrbitStoreWeb } from "./orbitStores/orbitStoreWeb";
 export { OrbitStoreInMemory } from "./orbitStores/orbitStoreInMemory";
