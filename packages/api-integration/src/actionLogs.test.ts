414b764293946ffb43ea1ed62cf0dced32032449
Ozzie Kirkby <ozzie.kirkby@gmail.com>
2021-07-27 18:00:33 -0700

    test /actionLogs validation and round-trip

---
+++ packages/api-integration/src/actionLogs.test.ts
@@ -0,0 +1,118 @@
+import { fetchRoute } from "./utils/fetchRoute";
+
+const VALID_ACTION_LOG = [
+  {
+    id: "z4EBG9jDqv5AQS3ivFTqFZcKomf2uSa3oZKbPv4k4U8AjVwp4CG",
+    data: {
+      timestampMillis: 15,
+      taskID: "12",
+      actionLogType: "ingest",
+      provenance: null,
+    },
+  },
+];
+
+it("round-trip request", async () => {
+  const { status: patchStatus, body: patchBody } = await fetchRoute(
+    `/api/actionLogs`,
+    {
+      method: "PATCH",
+      json: VALID_ACTION_LOG,
+      authorization: { token: "TEST" },
+    },
+  );
+  expect(patchStatus).toBe(204);
+  expect(patchBody).toBeUndefined();
+
+  const { status: getStatus, body: getBody } = await fetchRoute(
+    `/api/actionLogs`,
+    {
+      method: "GET",
+      authorization: { token: "TEST" },
+    },
+  );
+  expect(getStatus).toBe(200);
+  expect(getBody).toEqual({
+    objectType: "list",
+    hasMore: false,
+    data: [{ ...VALID_ACTION_LOG[0], objectType: "actionLog" }],
+  });
+});
+
+describe("[GET] validation", () => {
+  it("succeeds with valid parameters", async () => {
+    const { status } = await fetchRoute(
+      `/api/actionLogs?createdAfterID="someId"&limit=1000`,
+    );
+    expect(status).toBe(401);
+  });
+
+  it("it does not allow limit to be a negative integer", async () => {
+    const request = await fetchRoute(`/api/actionLogs?limit=-5`);
+    expect(request.status).toBe(400);
+    expect(request.body.errors).toEqual([
+      {
+        message: "query/limit must be >= 1",
+      },
+    ]);
+  });
+
+  it("it does not allow limit to be a floating point", async () => {
+    const request = await fetchRoute(`/api/actionLogs?limit=-5`);
+    expect(request.status).toBe(400);
+    expect(request.body.errors).toEqual([
+      {
+        message: "query/limit must be >= 1",
+      },
+    ]);
+  });
+});
+
+describe("[PATCH] validation", () => {
+  it("it fails when extra properties are provided", async () => {
+    const { status, body } = await fetchRoute(`/api/actionLogs`, {
+      method: "PATCH",
+      json: [{ ...VALID_ACTION_LOG[0], someExtraProperty: true }],
+    });
+    expect(status).toBe(400);
+    expect(body.errors).toEqual([
+      {
+        message: "body/0 must NOT have additional properties",
+      },
+    ]);
+  });
+
+  it("it fails when actionLogType is invalid", async () => {
+    const { status, body } = await fetchRoute(`/api/actionLogs`, {
+      method: "PATCH",
+      json: [
+        {
+          ...VALID_ACTION_LOG[0],
+          data: {
+            ...VALID_ACTION_LOG[0].data,
+            actionLogType: "invalid_action_log_type",
+          },
+        },
+      ],
+    });
+    expect(status).toBe(400);
+    expect(body.errors).toEqual([
+      {
+        message:
+          "body/0/data/actionLogType must be equal to one of the allowed values",
+      },
+      {
+        message: "body/0/data must have required property 'context'",
+      },
+      {
+        message: "body/0/data must have required property 'newTimestampMillis'",
+      },
+      {
+        message: "body/0/data must have required property 'parentActionLogIDs'",
+      },
+      {
+        message: "body/0/data must match a schema in anyOf",
+      },
+    ]);
+  });
+});
---
+++ packages/api-integration/src/utils/fetchRoute.ts
@@ -0,0 +1,36 @@
+import "isomorphic-fetch";
+
+const HOST: string = process.env.HOST!;
+if (!HOST) {
+  throw new Error("HOST must be specified");
+}
+
+type Args = { authorization?: { token: string } } & (
+  | { method: "GET" }
+  | { method: "POST"; json: any }
+  | { method: "PATCH"; json: any }
+);
+
+export async function fetchRoute(
+  path: string,
+  args: Args = { method: "GET", authorization: undefined },
+) {
+  const result = await fetch(`${HOST}${path}`, {
+    method: args.method,
+    body: "json" in args ? JSON.stringify(args.json) : null,
+    headers: {
+      ...("json" in args && {
+        "Content-Type": "application/json",
+      }),
+      ...(args.authorization && {
+        Authorization: `Token ${args.authorization.token}`,
+      }),
+    },
+  });
+  // helper to avoid having this boilerplate unwrapping within the tests
+  let body: any = undefined;
+  try {
+    body = await result.json();
+  } catch {}
+  return { status: result.status, body };
+}
