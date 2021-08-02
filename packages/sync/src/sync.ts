b38a31bf82352f9f8e6e1de1fb711e8111e17ac2
Andy Matuschak <andy@andymatuschak.org>
2021-08-02 12:37:08 -0700

    Adopted new metadata keys to implement incremental sync

--- packages/sync/src/sync.ts
+++ packages/sync/src/sync.ts
@@ -10,28 +10,63 @@ import { SyncAdapter } from "./syncAdapter";
 
 export async function syncOrbitStore(
   sourceStore: OrbitStore,
+
   destination: SyncAdapter,
-): Promise<void> {
-  const sourceSyncAdapter = new OrbitStoreSyncAdapter(sourceStore, "local");
+): Promise<{ sentEventCount: number; receivedEventCount: number }> {
+  const sourceSyncInterface = new OrbitStoreSyncAdapter(sourceStore, "local");
+
+  const latestSentEventIDKey = `__sync_${destination.id}_latestEventIDSent`;
+  const latestReceivedEventIDKey = `__sync_${destination.id}_latestEventIDReceived`;
+  const latestEventIDs = await sourceStore.database.getMetadataValues([
+    latestSentEventIDKey,
+    latestReceivedEventIDKey,
+  ]);
 
   // We use the same implementation for both sending and receiving events, switching the source/dest arguments appropriately.
-  // In the future there's an opportunity here to track which events originated locally vs. remotely, so that we don't end up telling the server about events it's already told us about, but in practice it's not a big deal: it'll just ignore them.
-  const sendPromise = sendNewEvents({
-    source: sourceSyncAdapter,
+  // High-level: we'll first send new events from the source to the destination, then dest->source, then source->dest again. The last step is necessary to bring the source up to the same final event ID as the destination.
+  // In the future there's an opportunity here to track which events originated locally vs. remotely, so that we don't end up telling the server about events it's already told us about, but in practice it's not a big deal: it'll just ignore them. I'm not yet hugely worried about marginal efficiency for this operation, so long as improvements are at least possible in principle with this architecture.
+
+  const sendArgs = {
+    source: sourceSyncInterface,
     destination: destination,
-    latestSentEventID: null,
-    setLatestSentEventID: () => Promise.resolve(),
+    latestSentEventID:
+      (latestEventIDs.get(latestSentEventIDKey) as EventID) ?? null,
+    setLatestSentEventID: (eventID: EventID) =>
+      sourceStore.database.setMetadataValues(
+        new Map([[latestSentEventIDKey, eventID]]),
+      ),
     batchSize: sendBatchSize,
-  });
-  const receivePromise = sendNewEvents({
+  };
+  const { sentEventCount: sentEventCount1, latestSentEventID } =
+    await sendNewEvents(sendArgs);
+  const { sentEventCount: receivedEventCount } = await sendNewEvents({
     source: destination,
-    destination: sourceSyncAdapter,
-    latestSentEventID: null,
-    setLatestSentEventID: () => Promise.resolve(),
+    destination: sourceSyncInterface,
+    latestSentEventID:
+      (latestEventIDs.get(latestReceivedEventIDKey) as EventID) ?? null,
+    setLatestSentEventID: (eventID) =>
+      sourceStore.database.setMetadataValues(
+        new Map([[latestReceivedEventIDKey, eventID]]),
+      ),
     batchSize: receiveBatchSize,
   });
 
-  await Promise.all([sendPromise, receivePromise]);
+  // After receiving, we send events again
+  const { sentEventCount: sentEventCount2 } = await sendNewEvents({
+    source: sourceSyncInterface,
+    destination: destination,
+    latestSentEventID, // use the latest sent event ID from the first step
+    setLatestSentEventID: (eventID: EventID) =>
+      sourceStore.database.setMetadataValues(
+        new Map([[latestSentEventIDKey, eventID]]),
+      ),
+    batchSize: sendBatchSize,
+  });
+
+  return {
+    sentEventCount: sentEventCount1 + sentEventCount2,
+    receivedEventCount,
+  };
 }
 
 async function sendNewEvents({
@@ -48,6 +83,7 @@ async function sendNewEvents({
   batchSize: number;
 }) {
   let afterEventID = latestSentEventID;
+  let sentEventCount = 0;
   while (true) {
     const events: Event[] = await source.listEvents(afterEventID, batchSize);
     if (events.length > 0) {
@@ -65,12 +101,15 @@ async function sendNewEvents({
       }
 
       await destination.putEvents(events);
+
+      sentEventCount += events.length;
       afterEventID = events[events.length - 1].id;
       await setLatestSentEventID(afterEventID);
     } else {
       break;
     }
   }
+  return { sentEventCount, latestSentEventID: afterEventID };
 }
 
 const receiveBatchSize = 500;
