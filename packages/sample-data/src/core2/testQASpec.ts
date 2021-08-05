ecdb60c3bee2af0c55ccb5cd13c5ded16ed3eb90
Ozzie Kirkby <ozzie.kirkby@gmail.com>
2021-08-05 12:31:36 -0700

    create standalone files for the cloze and QA spec

---
+++ packages/sample-data/src/core2/testQASpec.ts
@@ -0,0 +1,21 @@
+import {
+  MemoryTaskSpec,
+  QATaskContent,
+  TaskContentType,
+  TaskSpecType,
+} from "@withorbit/core2";
+
+export const testQASpec: MemoryTaskSpec<QATaskContent> = {
+  type: TaskSpecType.Memory,
+  content: {
+    type: TaskContentType.QA,
+    body: {
+      text: "Is it possible to use _quantum teleportation_ to transmit information faster than light?\n\nThis is a **second paragraph** with _**bold italic**_.",
+      attachments: [],
+    },
+    answer: {
+      text: "No.",
+      attachments: [],
+    },
+  },
+};
--- packages/sample-data/src/core2/testTasks.ts
+++ packages/sample-data/src/core2/testTasks.ts
@@ -3,50 +3,14 @@ import {
   AttachmentIngestEvent,
   AttachmentMIMEType,
   AttachmentReference,
-  ClozeTaskContent,
   EntityType,
   EventID,
   EventType,
-  MemoryTaskSpec,
   Task,
-  TaskContentType,
   TaskID,
   TaskIngestEvent,
-  TaskSpecType,
 } from "@withorbit/core2";
-
-const testClozeSpec: MemoryTaskSpec<ClozeTaskContent> = {
-  type: TaskSpecType.Memory,
-  content: {
-    type: TaskContentType.Cloze,
-    body: {
-      text: "This is a test cloze prompt.",
-      attachments: [],
-    },
-    components: {
-      a: {
-        order: 0,
-        ranges: [
-          {
-            startIndex: 5,
-            length: 5,
-            hint: null,
-          },
-        ],
-      },
-      b: {
-        order: 1,
-        ranges: [
-          {
-            startIndex: 2,
-            length: 2,
-            hint: null,
-          },
-        ],
-      },
-    },
-  },
-};
+import { testClozeSpec } from "./testClozeSpec";
 
 export const testTask: Task = {
   id: "a" as TaskID,
