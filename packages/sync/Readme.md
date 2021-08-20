035d7bbf15db358f44a1ad644af636e43ec7cebe
Andy Matuschak <andy@andymatuschak.org>
2021-08-19 17:17:21 -0700

    Added sync bin script and documentation

--- packages/sync/Readme.md
+++ packages/sync/Readme.md
@@ -2,7 +2,16 @@
 
 This package implements bidirectional syncing of `OrbitStore`s with a server (via `@withorbit/api-client` and `APISyncAdapter`) or another `OrbitStore` (via `OrbitStoreSyncAdapter`).
 
-Typical usage:
+To download or sync an on-disk store to/from the Orbit server from the command line, run `yarn sync`. Usage:
+* To sync the data of the default test user against a local emulated server:
+  * ```ORBIT_TOKEN=TEST yarn sync myData.orbitStore```
+  * If `myData.orbitStore` doesn't already exist, it will be created from the server's data.
+  * (this user has email `test@test.com` and password `test`)
+* To sync against a production user:
+  * Get a personal access token for your user (which you can generate by visiting `/settings?action=generatePersonalAccessToken` while logged in).
+  * Then run `ORBIT_ENV=production ORBIT_TOKEN=yourOrbitToken yarn sync someDataStore.orbitStore`
+
+Typical programmatic usage:
 ```
 import {APISyncAdapter, syncOrbitStore} from "@withorbit/sync"
 
---
+++ packages/sync/src/bin/run.ts
@@ -0,0 +1,51 @@
+import OrbitAPIClient, {
+  defaultAPIConfig,
+  emulatorAPIConfig,
+} from "@withorbit/api-client";
+import OrbitStoreFS from "@withorbit/store-fs";
+import { APISyncAdapter } from "../APISyncAdapter";
+import { syncOrbitStore } from "../sync";
+
+(async () => {
+  const personalAccessToken = process.env["ORBIT_TOKEN"];
+  if (!personalAccessToken) {
+    console.error(
+      "Must provide personal access token via TOKEN environment variable",
+    );
+    process.exit(0);
+  }
+
+  const apiConfig =
+    process.env["ORBIT_ENV"] && process.env["ORBIT_ENV"] === "production"
+      ? defaultAPIConfig
+      : emulatorAPIConfig;
+
+  const orbitStorePath = process.argv[2];
+  if (!orbitStorePath) {
+    console.error(
+      "Usage: ORBIT_TOKEN=your_personal_access_token ORBIT_ENV=[development/production] yarn sync pathToLocalStore.orbitStore",
+    );
+    process.exit(0);
+  }
+
+  const apiClient = new OrbitAPIClient(
+    async () => ({ personalAccessToken }),
+    apiConfig,
+  );
+  const apiAdapter = new APISyncAdapter(apiClient, "remote");
+  const localStore = await OrbitStoreFS.open(orbitStorePath, true);
+
+  await syncOrbitStore({
+    source: localStore,
+    destination: apiAdapter,
+    sendBatchSize: 200,
+    receiveBatchSize: 5000,
+  });
+  await localStore.close();
+  process.exit(0);
+})()
+  .then(() => {
+    console.log("Done");
+    process.exit(0);
+  })
+  .catch(console.error);
