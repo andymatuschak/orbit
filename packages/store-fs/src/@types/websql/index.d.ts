aef2d29dcdadf1c31ea43a3dcbe948f0bf84be2a
Andy Matuschak <andy@andymatuschak.org>
2021-10-19 17:07:35 -0700

    Migrated from expo-sqlite to react-native-quick-sqlite

--- packages/store-fs/src/@types/websql/index.d.ts
+++ packages/store-fs/src/@types/websql/index.d.ts
@@ -7,3 +7,38 @@ declare module "websql" {
   ): import("../../sqlite/types").SQLDatabase;
   export = openDatabase;
 }
+
+declare module "websql/custom" {
+  namespace customOpenDatabase {
+    export interface WebSQLQuery {
+      sql: string;
+      args: any[];
+    }
+
+    export type WebSQLResult = {
+      error: Error;
+      insertId: undefined;
+      rowsAffected: undefined;
+      rows: undefined;
+    } | {
+      error: undefined;
+      insertId?: number;
+      rowsAffected: number;
+      rows: unknown[];
+    }
+
+    export class WebSQLCustomDatabase {
+      constructor(name: string);
+
+      exec(
+        queries: WebSQLQuery[],
+        readOnly: boolean,
+        callback: (err: Error | undefined, results: WebSQLResult[]) => void,
+      ): void;
+    }
+  }
+  function customOpenDatabase(
+    constructor: typeof customOpenDatabase.WebSQLCustomDatabase,
+  ): (name: string) => import("../../sqlite/types").SQLDatabase;
+  export = customOpenDatabase;
+}
--- packages/store-fs/src/sqlite/binding.native.ts
+++ packages/store-fs/src/sqlite/binding.native.ts
@@ -1,9 +1,52 @@
 // This file and databaseBinding.ts provide a unified binding for the external SQLite database APIs we use on those platforms.
 // This file supplies the implementation used when running in a React Native  environment.
 
-import * as SQLite from "expo-sqlite";
 import { SQLDatabase } from "./types";
+import "react-native-quick-sqlite";
+import CustomWebSQLDatabase from "websql/custom";
+
+class RNSQLiteDatabase {
+  private readonly _name: string;
+
+  constructor(name: string) {
+    this._name = name;
+    sqlite.open(name);
+  }
+
+  exec(
+    queries: CustomWebSQLDatabase.WebSQLQuery[],
+    readOnly: boolean,
+    callback: (err: any, results: CustomWebSQLDatabase.WebSQLResult[]) => void,
+  ) {
+    // TODO: check queries vs. readOnly flag
+    const results = new Array<CustomWebSQLDatabase.WebSQLResult>(
+      queries.length,
+    );
+    for (const query of queries) {
+      try {
+        const result = sqlite.executeSql(this._name, query.sql, query.args);
+        results.push({
+          insertId: result.insertId,
+          rowsAffected: result.rowsAffected,
+          rows: result.rows._array,
+          error: undefined,
+        });
+      } catch (error) {
+        results.push({
+          error,
+          rows: undefined,
+          rowsAffected: undefined,
+          insertId: undefined,
+        });
+      }
+    }
+
+    callback(undefined, results);
+  }
+}
+
+const openRNSQLiteDatabase = CustomWebSQLDatabase(RNSQLiteDatabase);
 
 export function openDatabase(subpath: string): SQLDatabase {
-  return SQLite.openDatabase(subpath, "", "", 1);
+  return openRNSQLiteDatabase(subpath);
 }
