6d548d9047c57ad37943ee462003dd1b8537c95d
Andy Matuschak <andy@andymatuschak.org>
2021-10-19 17:11:01 -0700

    Added migration for attachment table

---
+++ packages/store-fs/src/sqlite/migrations/20211019170802_createAttachmentsTable.ts
@@ -0,0 +1,15 @@
+import { SQLMigration } from "./migrationType";
+
+const migration: SQLMigration = {
+  version: 20211019170802,
+  statements: [
+    `
+    CREATE TABLE attachments (
+      id TEXT PRIMARY KEY,
+      data BLOB NOT NULL,
+      mimeType TEXT NOT NULL
+    )
+    `
+  ],
+};
+export default migration;
--- packages/store-fs/src/sqlite/migrations/index.ts
+++ packages/store-fs/src/sqlite/migrations/index.ts
@@ -1,10 +1,12 @@
 import migration_20210612111147_createMetadataTable from "./20210612111147_createMetadataTable";
 import migration_20210612112129_initialSchema from "./20210612112129_initialSchema";
+import migration_20211019170802_createAttachmentsTable from "./20211019170802_createAttachmentsTable";
 
 // Should be sorted by version number.
 export const migrations = [
   migration_20210612111147_createMetadataTable,
   migration_20210612112129_initialSchema,
+  migration_20211019170802_createAttachmentsTable,
 ];
 export const latestSchemaVersionNumber =
   migrations[migrations.length - 1].version;
