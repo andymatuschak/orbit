9ce62b57d0d28ddd75bdad7b60a8031eb56b1803
Ozzie Kirkby <ozzie.kirkby@gmail.com>
2022-09-08 00:12:26 +0200

    add raw markdown ingest case

--- packages/interpreter/src/interpreters/markdown/MarkdownInterpreter.test.ts
+++ packages/interpreter/src/interpreters/markdown/MarkdownInterpreter.test.ts
@@ -27,3 +27,27 @@ This is a {test} cloze prompt
   expect(ingestible.sources).toHaveLength(1);
   expect(ingestible.sources[0]).toMatchSnapshot();
 });
+
+it("interprets raw markdown file with mixed content", async () => {
+  const file: InterpretableFile = {
+    name: "Hello World.md",
+    path: "/some/file/path/Hello World.md",
+    content: `# Hello World
+
+This is an example document which will contain both normal prompts and cloze prompts.
+    
+Q. What is the influence of A on B?
+A. It makes B harder to read w.r.t A
+    
+This is another paragraph.
+    
+Q. This is another prompt.
+A. Which contains a completely valid answer.
+    
+This is a {test} cloze prompt`,
+  };
+  const interpreter = new MarkdownInterpreter(CryptoBase64Hasher);
+  const ingestible = await interpreter.interpret([file]);
+  expect(ingestible.sources).toHaveLength(1);
+  expect(ingestible.sources[0]).toMatchSnapshot();
+});
