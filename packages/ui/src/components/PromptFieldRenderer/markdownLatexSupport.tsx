46173ac607877ee20621cc593850ff56954d86ec
Andy Matuschak <andy@andymatuschak.org>
2023-05-17 10:53:31 -0700

    andymatuschak/orbit#153 Implement react-native support for LaTeX
    Made a solid start here. Needs polish and alignment work still.

--- packages/ui/src/components/PromptFieldRenderer/markdownLatexSupport.tsx
+++ packages/ui/src/components/PromptFieldRenderer/markdownLatexSupport.tsx
@@ -1,9 +1,14 @@
+import _katex from "katex";
+import * as MarkdownItTexMath from "markdown-it-texmath";
+import React from "react";
 import * as MarkdownDisplay from "react-native-markdown-display";
+import MathJax from "react-native-mathjax-svg";
+import * as colors from "../../styles/colors";
 
 export async function addLatexSupport(
   markdownIt: MarkdownDisplay.MarkdownIt, // eslint-disable-line @typescript-eslint/no-unused-vars
 ): Promise<void> {
-  // TODO
+  markdownIt.use(MarkdownItTexMath.use(_katex));
 }
 
 export const renderInlineMath: MarkdownDisplay.RenderFunction = (
@@ -13,7 +18,15 @@ export const renderInlineMath: MarkdownDisplay.RenderFunction = (
   styles,
   inheritedStyles = {}, // eslint-disable-line @typescript-eslint/no-unused-vars
 ) => {
-  return node.content;
+  return (
+    <MathJax
+      fontSize={inheritedStyles.fontSize * 0.83}
+      color={colors.ink}
+      display={false}
+    >
+      {node.content}
+    </MathJax>
+  );
 };
 
 export const renderBlockMath: MarkdownDisplay.RenderFunction = (
@@ -23,5 +36,13 @@ export const renderBlockMath: MarkdownDisplay.RenderFunction = (
   styles,
   inheritedStyles = {}, // eslint-disable-line @typescript-eslint/no-unused-vars
 ) => {
-  return node.content;
+  return (
+    <MathJax
+      fontSize={inheritedStyles.fontSize * 0.83}
+      color={colors.ink}
+      display={true}
+    >
+      {node.content}
+    </MathJax>
+  );
 };
