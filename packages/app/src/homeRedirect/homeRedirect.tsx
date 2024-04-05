0ae7dabd98a6f8fee05b2e98766886dfb6a234ef
Andy Matuschak <andy@andymatuschak.org>
2024-04-05 10:08:00 -0700

    Fixed / -> /review redirect for native

--- packages/app/src/homeRedirect/homeRedirect.tsx
+++ packages/app/src/homeRedirect/homeRedirect.tsx
@@ -1,3 +1,8 @@
+import { useRouter } from "expo-router";
+
 // Expo Router doesn't support platform extensions in its page-based routing. Instead, we have to support some non-route component which has the extensions.
 // https://docs.expo.dev/router/advanced/platform-specific-modules/#platform-specific-extensions
-export { default } from "../app/review.js";
+export default function Page() {
+  const router = useRouter();
+  router.push("/review");
+}
