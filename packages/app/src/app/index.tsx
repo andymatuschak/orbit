d878e9f1d742823c06b01bf6aefa91160980574d
Andy Matuschak <andy@andymatuschak.org>
2024-04-05 08:42:16 -0700

    Fixed / incorrectly redirecting to /review on web

--- packages/app/src/app/index.tsx
+++ packages/app/src/app/index.tsx
@@ -1,1 +1,1 @@
-export { default } from "./review.js";
+export { default } from "../homeRedirect/homeRedirect.js";
---
+++ packages/app/src/homeRedirect/homeRedirect.web.tsx
@@ -0,0 +1,3 @@
+// Expo Router doesn't support platform extensions in its page-based routing. Instead, we have to support some non-route component which has the extensions.
+// https://docs.expo.dev/router/advanced/platform-specific-modules/#platform-specific-extensions
+export { default } from "../app/learnMore.js";
